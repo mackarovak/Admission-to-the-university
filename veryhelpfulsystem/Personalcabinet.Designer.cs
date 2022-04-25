438f62f7bf55a55d25151b80e91d7b64ff06db5b
nsttmf14 <ntim14@yandex.ru>
2022-04-25 19:04:58 +0300

    изменен ЛК

--- veryhelpfulsystem/Personalcabinet.Designer.cs
+++ veryhelpfulsystem/Personalcabinet.Designer.cs
@@ -47,32 +47,40 @@ namespace veryhelpfulsystem
             this.tabPage7 = new System.Windows.Forms.TabPage();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.exit = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label7 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.PageCabinet.SuspendLayout();
             this.tabControl2.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage4.SuspendLayout();
+            this.tabPage5.SuspendLayout();
             this.tabPage7.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.PageCabinet);
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(884, 453);
+            this.tabControl1.Size = new System.Drawing.Size(1137, 601);
             this.tabControl1.TabIndex = 0;
             // 
             // PageCabinet
             // 
             this.PageCabinet.Controls.Add(this.tabControl2);
             this.PageCabinet.Location = new System.Drawing.Point(4, 22);
-            this.PageCabinet.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.PageCabinet.Margin = new System.Windows.Forms.Padding(2);
             this.PageCabinet.Name = "PageCabinet";
-            this.PageCabinet.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.PageCabinet.Size = new System.Drawing.Size(876, 427);
+            this.PageCabinet.Padding = new System.Windows.Forms.Padding(2);
+            this.PageCabinet.Size = new System.Drawing.Size(1129, 575);
             this.PageCabinet.TabIndex = 0;
             this.PageCabinet.Text = "Мой кабинет";
             this.PageCabinet.UseVisualStyleBackColor = true;
@@ -85,7 +93,7 @@ namespace veryhelpfulsystem
             this.tabControl2.Controls.Add(this.tabPage6);
             this.tabControl2.Controls.Add(this.tabPage7);
             this.tabControl2.Location = new System.Drawing.Point(0, 0);
-            this.tabControl2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabControl2.Margin = new System.Windows.Forms.Padding(2);
             this.tabControl2.Name = "tabControl2";
             this.tabControl2.SelectedIndex = 0;
             this.tabControl2.Size = new System.Drawing.Size(1181, 511);
@@ -93,19 +101,21 @@ namespace veryhelpfulsystem
             // 
             // tabPage1
             // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.exit);
             this.tabPage1.Controls.Add(this.FIOtext);
             this.tabPage1.Controls.Add(this.IDtext);
             this.tabPage1.Controls.Add(this.label2);
             this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
             this.tabPage1.Size = new System.Drawing.Size(1173, 485);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Анкета";
-            this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
@@ -113,24 +123,24 @@ namespace veryhelpfulsystem
             this.label3.Location = new System.Drawing.Point(15, 85);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.Size = new System.Drawing.Size(47, 18);
             this.label3.TabIndex = 6;
             this.label3.Text = "ФИО";
             // 
             // FIOtext
             // 
-            this.FIOtext.Location = new System.Drawing.Point(178, 81);
-            this.FIOtext.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.FIOtext.Location = new System.Drawing.Point(75, 85);
+            this.FIOtext.Margin = new System.Windows.Forms.Padding(2);
             this.FIOtext.Name = "FIOtext";
-            this.FIOtext.Size = new System.Drawing.Size(231, 20);
+            this.FIOtext.Size = new System.Drawing.Size(231, 24);
             this.FIOtext.TabIndex = 5;
             // 
             // IDtext
             // 
-            this.IDtext.Location = new System.Drawing.Point(178, 25);
-            this.IDtext.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.IDtext.Location = new System.Drawing.Point(209, 25);
+            this.IDtext.Margin = new System.Windows.Forms.Padding(2);
             this.IDtext.Name = "IDtext";
-            this.IDtext.Size = new System.Drawing.Size(97, 20);
+            this.IDtext.Size = new System.Drawing.Size(97, 24);
             this.IDtext.TabIndex = 4;
             // 
             // label2
@@ -139,7 +149,7 @@ namespace veryhelpfulsystem
             this.label2.Location = new System.Drawing.Point(15, 70);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.Size = new System.Drawing.Size(0, 18);
             this.label2.TabIndex = 1;
             // 
             // label1
@@ -148,44 +158,48 @@ namespace veryhelpfulsystem
             this.label1.Location = new System.Drawing.Point(15, 25);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.Size = new System.Drawing.Size(176, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "Индивидуальный код";
             // 
             // tabPage4
             // 
+            this.tabPage4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tabPage4.Controls.Add(this.flowLayoutPanel1);
+            this.tabPage4.Controls.Add(this.label5);
             this.tabPage4.Controls.Add(this.textIndivid);
             this.tabPage4.Controls.Add(this.label4);
+            this.tabPage4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.tabPage4.Location = new System.Drawing.Point(4, 22);
-            this.tabPage4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage4.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Size = new System.Drawing.Size(1173, 485);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "ИД";
-            this.tabPage4.UseVisualStyleBackColor = true;
             // 
             // textIndivid
             // 
-            this.textIndivid.Location = new System.Drawing.Point(290, 7);
-            this.textIndivid.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textIndivid.Location = new System.Drawing.Point(445, 56);
+            this.textIndivid.Margin = new System.Windows.Forms.Padding(2);
             this.textIndivid.Name = "textIndivid";
-            this.textIndivid.Size = new System.Drawing.Size(68, 20);
+            this.textIndivid.Size = new System.Drawing.Size(68, 24);
             this.textIndivid.TabIndex = 1;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(11, 9);
+            this.label4.Location = new System.Drawing.Point(3, 56);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(272, 13);
+            this.label4.Size = new System.Drawing.Size(423, 18);
             this.label4.TabIndex = 0;
             this.label4.Text = "Количество баллов за индивидуальные достижения";
             // 
             // tabPage5
             // 
+            this.tabPage5.Controls.Add(this.flowLayoutPanel3);
             this.tabPage5.Location = new System.Drawing.Point(4, 22);
-            this.tabPage5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage5.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage5.Name = "tabPage5";
             this.tabPage5.Size = new System.Drawing.Size(1173, 485);
             this.tabPage5.TabIndex = 4;
@@ -195,7 +209,7 @@ namespace veryhelpfulsystem
             // tabPage6
             // 
             this.tabPage6.Location = new System.Drawing.Point(4, 22);
-            this.tabPage6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage6.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage6.Name = "tabPage6";
             this.tabPage6.Size = new System.Drawing.Size(1173, 485);
             this.tabPage6.TabIndex = 5;
@@ -206,7 +220,7 @@ namespace veryhelpfulsystem
             // 
             this.tabPage7.Controls.Add(this.listBox1);
             this.tabPage7.Location = new System.Drawing.Point(4, 22);
-            this.tabPage7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage7.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage7.Name = "tabPage7";
             this.tabPage7.Size = new System.Drawing.Size(1173, 485);
             this.tabPage7.TabIndex = 6;
@@ -215,7 +229,10 @@ namespace veryhelpfulsystem
             // 
             // listBox1
             // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
             this.listBox1.Items.AddRange(new object[] {
             "",
             resources.GetString("listBox1.Items"),
@@ -244,31 +261,80 @@ namespace veryhelpfulsystem
             "6. Оригинал договора на бумажном носителе будет подписан сторонами в начале учебн" +
                 "ого года"});
             this.listBox1.Location = new System.Drawing.Point(5, 5);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(1034, 459);
+            this.listBox1.Size = new System.Drawing.Size(1061, 468);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // exit
             // 
-            this.exit.Location = new System.Drawing.Point(888, 22);
-            this.exit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.exit.Location = new System.Drawing.Point(1012, 8);
+            this.exit.Margin = new System.Windows.Forms.Padding(2);
             this.exit.Name = "exit";
-            this.exit.Size = new System.Drawing.Size(83, 38);
+            this.exit.Size = new System.Drawing.Size(109, 35);
             this.exit.TabIndex = 1;
             this.exit.Text = "Выход";
             this.exit.UseVisualStyleBackColor = true;
             this.exit.Click += new System.EventHandler(this.exit_Click);
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(851, 18);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Индивидуальные достижения для поступающих на обучение по программам бакалавриата " +
+    "и специалитета";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label6);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(6, 90);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1105, 392);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(3, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(1098, 576);
+            this.label6.TabIndex = 0;
+            this.label6.Text = resources.GetString("label6.Text");
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Controls.Add(this.label7);
+            this.flowLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(1173, 485);
+            this.flowLayoutPanel3.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(1166, 300);
+            this.label7.TabIndex = 0;
+            this.label7.Text = resources.GetString("label7.Text");
+            // 
             // Personalcabinet
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1283, 682);
-            this.Controls.Add(this.exit);
             this.Controls.Add(this.tabControl1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Personalcabinet";
             this.Text = "Личный кабинет";
             this.Load += new System.EventHandler(this.Personalcabinet_Load);
@@ -279,7 +345,12 @@ namespace veryhelpfulsystem
             this.tabPage1.PerformLayout();
             this.tabPage4.ResumeLayout(false);
             this.tabPage4.PerformLayout();
+            this.tabPage5.ResumeLayout(false);
             this.tabPage7.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -303,5 +374,10 @@ namespace veryhelpfulsystem
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.TextBox textIndivid;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Label label7;
     }
 }
